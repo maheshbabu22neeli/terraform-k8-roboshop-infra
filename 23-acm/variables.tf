8e7fb5a68e8a7dc74f2ac20ae41f919e10975a26
Mahesh Babu Neeli <maheshbabu22.neeli@gmail.com>
2026-04-26 18:00:32 +1000

    Terraform K8: ACM Configuration

---
+++ 23-acm/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  type    = string
+  default = "Z013175831RO1NWFBESW7"
+}
+
+variable "domain_name" {
+  type    = string
+  default = "neeli.online"
+}
